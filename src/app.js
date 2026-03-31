3fddcff39efb0edfb8eaf7ff2729a1866e3d62a0
RachmadFidhani <simokart3@gmail.com>
2026-03-31 09:54:02 +0700

    add openapi yaml and fix image url

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const routes = require('./routes');
 
 // Load environment variables
@@ -12,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'public', 'uploads')));
+app.use(express.static('public'));
 
 // Routes
 app.use('/api', routes);
